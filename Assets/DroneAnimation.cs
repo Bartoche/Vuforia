c057f57e0428fa1bbc2c3ebca2926308fd5f3232
Thomas <thomas.barthelemy@eleves.ec-nantes.fr>
2019-03-29 17:46:53 +0100

    initial commit

---
+++ Assets/DroneAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneAnimation : MonoBehaviour {
+
+    private Animator m_Animator;
+    private bool facing = true;
+    private bool scan = false;
+    private bool showlaser = false;
+
+
+	// Use this for initialization
+	void Start () {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Toggle Scanning");
+            scan = !scan;
+            showlaser = !showlaser;
+            m_Animator.SetBool("IsScanning", scan);
+            m_Animator.SetBool("IsShowingLaser", showlaser);
+        }
+    }
+}
